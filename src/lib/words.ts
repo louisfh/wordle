12fab101792896ec8f2b73c3f62ef1192a2811cf
Louis Freeland-Haynes <louis.freelandhaynes@gmail.com>
2022-01-23 13:28:52 -0500

    Make random word, not 1 word per day

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -17,7 +17,7 @@ export const getWordOfDay = () => {
   const epochMs = 1641013200000
   const now = Date.now()
   const msInDay = 86400000
-  const index = Math.floor((now - epochMs) / msInDay)
+  const index = Math.floor(Math.random()*70)
 
   return {
     solution: WORDS[index].toUpperCase(),
